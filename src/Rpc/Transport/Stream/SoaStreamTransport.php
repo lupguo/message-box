40519a23632b25b900007007fbb69092861fdf35
Terry.atHome <tkstorm@163.com>
2017-11-20 01:08:12 +0800

    1. 更新了composer.json中的错误； 2. 补签SoaStreamTransport.php类的构造方法；

--- src/Rpc/Transport/Stream/SoaStreamTransport.php
+++ src/Rpc/Transport/Stream/SoaStreamTransport.php
@@ -15,7 +15,18 @@ use Rpc\Exceptions\TransportException;
 
 class SoaStreamTransport extends StreamTransport
 {
-    
+    /**
+     * SoaStreamTransport constructor.
+     *
+     * @param string $remoteIp
+     * @param int $port
+     * @param int $connectTimeout
+     */
+    public function __construct($remoteIp = '127.0.0.1', $port = 43210, $connectTimeout = 3)
+    {
+        parent::__construct($remoteIp, $port, $connectTimeout);
+    }
+
     /**
      * 模拟Obs从流中读取对应的资源
      *
@@ -23,10 +34,10 @@ class SoaStreamTransport extends StreamTransport
      */
     public function readData()
     {
-        if ( !is_resource($this->resource)) {
+        if (!is_resource($this->resource)) {
             throw new TransportException("IN RPC TRANSPORT , READ RESOURCE IS UNAVAILABLE !!");
         }
-    
+
         $maxByteSize = 5;
         $length = 0;
         $content = fread($this->resource, $maxByteSize);
@@ -39,11 +50,11 @@ class SoaStreamTransport extends StreamTransport
             throw new TransportException('IN RPC TRANSPORT , READ TAG LENGTH ERROR !!');
         }
         $content .= stream_get_contents($this->resource, $length);
-    
+
         return $content;
     }
-    
-    
+
+
     /**
      * 安装SOA对接流规则，在传输数据前追加对应的的字节长度
      *
@@ -54,16 +65,16 @@ class SoaStreamTransport extends StreamTransport
      */
     public function writeData($data)
     {
-        if ( !is_resource($this->resource) ) {
+        if (!is_resource($this->resource)) {
             throw new TransportException("IN RPC TRANSPORT , WRITE RESOURCE IS UNAVAILABLE !!");
         }
-        
+
         if (empty($data))
             throw new TransportException("EMPTY REQUEST BODY .");
-        
+
         //add pack length
-        $data = $this->encodeLength(strlen($data)). $data;
-        
+        $data = $this->encodeLength(strlen($data)) . $data;
+
         for ($written = 0; $written < strlen($data); $written += $fwrite) {
             $fwrite = fwrite($this->resource, substr($data, $written), $this->writeBuffer);
             if ($fwrite === false) {
@@ -71,7 +82,7 @@ class SoaStreamTransport extends StreamTransport
             }
         }
     }
-    
+
     /**
      * obs的获取二进制字符串前面加上其长度算法字节
      *
@@ -81,7 +92,7 @@ class SoaStreamTransport extends StreamTransport
     private function encodeLength($value)
     {
         $dataLengthByte = '';
-        while (true){
+        while (true) {
             if (($value & ~0x7F) == 0) {
                 $dataLengthByte .= pack('c', $value);
                 break;
@@ -91,7 +102,7 @@ class SoaStreamTransport extends StreamTransport
                 $value = $value >> 7;
             }
         }
-        
+
         return $dataLengthByte;
     }
 }
